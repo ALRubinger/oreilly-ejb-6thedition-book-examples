2f3e1a7aa99faed195377fd9d2fcdae87cd49834
Andrew Lee Rubinger <alr@jboss.org>
2010-05-19 23:56:54 +0000

    [EJBBOOK-27] Fix commment only

--- chxx-employeeregistry/src/main/java/org/jboss/ejb3/examples/employeeregistry/chyy/mapping/EmployeeWithEmbeddedPK.java
+++ chxx-employeeregistry/src/main/java/org/jboss/ejb3/examples/employeeregistry/chyy/mapping/EmployeeWithEmbeddedPK.java
@@ -43,7 +43,7 @@ public class EmployeeWithEmbeddedPK
    //-------------------------------------------------------------------------------------||
 
    /**
-    * Last Name
+    * Primary key, composite and embedded
     */
    @EmbeddedId
    private EmbeddedEmployeePK id;
